f03120f9efd79fa5beb0e66fbf46641abcbba91f
Sommy <amaugosomto@gmail.com>
2018-06-27 23:50:07 +0100

    Added new functions and fixed some bugs

--- routes/loans.js
+++ routes/loans.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const LoanController = require('../controllers/loan.controller');
-// const UserValidator = require('../validators/loan.validator');
+const LoanController = require('../controllers/loans.controller');
 var JWT_Verify = require('../middlewares/jwt');
 
 router.use(JWT_Verify.verifyToken);
 router.post('/request',LoanController.RequestLoan);
+router.get('/:id', LoanController.ViewLoan);
 router.get('/',LoanController.ViewLoans);
+router.get('/approve/:id',LoanController.GuarantorApprove);
 router.get('/accept/:id', LoanController.AcceptLoan);
 router.get('/reject/:id', LoanController.RejectLoan);
 router.delete('/delete/:id', LoanController.DeleteLoan);
 router.put('/disburse/:id', LoanController.DisburseLoan);
-router.get('/repay/:id', LoanController.LoanRepaid);
+router.get('/repaid/:id', LoanController.LoanRepaid);
+router.post('/repay/:id', LoanController.LoanRepay);
 
 module.exports = router;
