c25744861d2dd4001b315603c323275ccc34a8ab
Queen Adydams <adydams@gmail.com>
2018-07-04 18:00:33 -1100

    modified payload into an array to loop through in test

--- __tests__/api/users_login.js
+++ __tests__/api/users_login.js
@@ -1,5 +1,5 @@
 const frisby = require('frisby');
-
+const Joi = frisby.Joi;
 //testing login route with valid values
 it ('should return status 200', function () {
   return frisby
@@ -9,7 +9,17 @@ it ('should return status 200', function () {
 
     })
     .expect('status', 200)
-});
+    .expect('json', 'status', true, 'message', 'success' )
+    .expect('jsonTypes', 'payload.*', { // Assert *each* object in 'items' array
+      'token': Joi.string(),
+      'user_details': Joi.array(),
+    }, 'user_details.*',{
+        'id': Joi.string().required(),
+      'email': Joi.string().email().required(),
+      'phoneNumber': Joi.string().required().max(11),
+     })
+    });
+
 
 //testing login route with invalid password
 it ('should return status 400', function () {
@@ -20,5 +30,6 @@ it ('should return status 400', function () {
 
     })
     .expect('status', 400)
+    .expect('json', 'status', false, 'message', 'Incorrect login credentials, please try again')
 });
 
